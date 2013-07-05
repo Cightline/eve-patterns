182b49806a33bdbd81bdb58f90a58ce8c7aafaf6
Afterburn <mujahideen.solutions@gmail.com>
2013-07-04 18:03:46 -0600

    ..

--- informative.py
+++ informative.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*- 
 
-import urllib.request
-import urllib.parse
+import urllib
+import ConfigParser
+import os
 
 import xml.etree.ElementTree as ET
 
@@ -12,13 +13,13 @@ import sqlite3
 class eve_api():
     def __init__(self):
         self.api_key = {"keyID":None, "vCode":None}
-        self.db_names = ["transactions_db","journal_db"]
         self.connected = False
         self.keys = []
         self.encoding = None
         self.transactions = {}
         self.db = None
         self.cursor = None
+        self.config = ConfigParser.ConfigParser()
         self.urls = {"base":"https://api.eveonline.com",
                      "account_balance":"/char/AccountBalance.xml.aspx",
                      "wallet_transactions":"/char/WalletTransactions.xml.aspx",
@@ -27,23 +28,39 @@ class eve_api():
                      "asset_list":"/char/AssetList.xml.aspx"}
 
 
-    def import_api_key(self, path):
-        with open(path, "r") as key_file:
-            self.api_key["keyID"], self.api_key["vCode"] = key_file.read().split()
+
+    def import_api_key(self, path='config'):
+        self.config.read(path)
+
+        self.api_key['keyID'] = self.config.get('options','keyid')
+        self.api_key['vCode'] = self.config.get('options', 'vcode')
+        
 
 
-    def connect_db(self, name):
+    def connect_db(self, name='data.db'):
+        create = False
+
+        # See if the database exists first
+        if not os.path.exists(name):
+            create = True
+           
+        # Connect/create
         if not self.db:
             self.db = sqlite3.connect(name)
             self.cursor = self.db.cursor()
             self.connected = True
+
+        # Create tables (if they need to be)
+        if create:
+            self.ct_trans()
+            self.ct_journal()
 
     def disconnect_db(self):
         if self.db:
             self.db.disconnect()
             self.connected = False
 
-    def create_transactions_table(self):
+    def ct_trans(self):
         self.cursor.execute('''create table if not exists transactions
                                (price float, clientName text, transactionType text, 
                                journalTransactionID int, stationID int, transactionFor text, 
@@ -52,27 +69,32 @@ class eve_api():
                                typeName text, stationName text)''')
 
 
-    def create_journal_table(self):
+    def ct_journal(self):
         self.cursor.execute('''create table if not exists journal
                                 (argName1 text, ownerID1 int, ownerID2 int, date datetime primary key, 
                                 taxAmount float, ownerName1 text, reason text, 
                                 taxReceiverID int, ownerName2 text, amount float, refTypeID int, refID int, 
                                 argID1 int, balance float)''')
 
+    def ct_char_assets(self):
+        self.cursor.execute('''create table if not exists char_assets 
+                               (itemID int, typeID int, quantity int, flag int, singleton int, name text''')
+                                
     def encode_data(self, data):
-        return bytes(urllib.parse.urlencode(data), 'utf-8')
+        return urllib.urlencode(data)
 
 
     def get_page(self, short_url, use_api_key=True):
         url = "%s/%s" % (self.urls["base"], self.urls[short_url])
 
         if use_api_key != True:
-            return self.manage_xml(urllib.request.urlopen(url))
+            return self.manage_xml(urllib.urlopen(url))
         else:
-            return self.manage_xml(urllib.request.urlopen(url, self.encode_data(self.api_key)))
+            return self.manage_xml(urllib.urlopen(url, self.encode_data(self.api_key)))
 
 
     def manage_xml(self, page):
+        #print(page.read())
         return ET.fromstring(page.read())
 
 
@@ -139,16 +161,31 @@ class eve_api():
         root.clear()
         self.db.commit()
 
+
+
     def import_assets(self):
+        #{'columns': 'itemID,locationID,typeID,quantity,flag,singleton', 'name': 'assets', 'key': 'itemID'}
         root = self.get_page("asset_list")
+
+        #for item in list(root):
+        #    if item.tag == 'cachedUntil':
+        #        print(item.attrib)
+        #        print(item.text)
+
+        for item in root.iter('row'):
+            print(item.attrib)
+        
 
     #def calculate_job_profit(self, 
     #def calc_brokers_fee(self):
     #    fee = 1.000 - (0.050 * self.skill["b
 
+
+
 if __name__ == "__main__":
     api = eve_api()
-    api.import_api_key('api_key')
-    api.connect_db('transactions.db')
+    api.import_api_key()
+    api.connect_db()
     api.import_journal()
     api.import_transactions()
+    #api.import_assets()
